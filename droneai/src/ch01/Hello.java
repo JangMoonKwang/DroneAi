9f0d99f5a2c37be00d6973ae633d874c8576cb3f
SDENG <ebsit@naver.com>
2026-05-28 20:17:58 +0900

    first

---
+++ droneai/src/ch01/Hello.java
@@ -0,0 +1,12 @@
+package ch01;
+
+class Hello {
+    // Your program begins with a call to main()
+    
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        
+        System.out.println("Hello, World");
+    }
+}
